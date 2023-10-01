5eae90b811b702f2f3a41c54ea0824e12b54f538
ALFAROSO <87240930+ALFAROSO@users.noreply.github.com>
2023-09-30 17:33:24 -0700

    Update index.js

--- A1_Alberth_Rodriguez/index.js
+++ A1_Alberth_Rodriguez/index.js
@@ -1,62 +1,80 @@
-
 class Food {//create the class with the constructor
     constructor(name, carbs, fat, protein) {
-        this.name = name;
-        this.carbs = carbs;
-        this.fat = fat;
-        this.protein = protein;
+        this.name = name
+        this.carbs = carbs
+        this.fat = fat
+        this.protein = protein
+    }
+
+    static gethi(Foods) {
+        if (Foods.length > 0) {//to check if the array is still empty
+
+            let highest_Carb = Foods[0]//create varible to store the object highest_Carb
+            let highest_Fat = Foods[0]//create varible to store the object highest_Fat
+            let highest_Protein = Foods[0]//create varible to store the object highest_Protein
+
+            for (const food of Foods) {// Calculate the objects with highest values
+                if (food.carbs > highest_Carb.carbs) {
+                    highest_Carb = food
+                }
+                if (food.fat > highest_Fat.fat) {
+                    highest_Fat = food
+                }
+                if (food.protein > highest_Protein.protein) {
+                    highest_Protein = food
+                }
+            }
+            return { highest_Carb, highest_Fat, highest_Protein }
+        } else {
+            window.alert("No food item stored yet.")
+        }
     }
 }
 
-const Foods = [];// Empty array to store food objects
-const submit_Btn = document.getElementById("form_area");
-const calculate_highestBtn = document.getElementById("calculate_highest");
-const info_area = document.getElementById("info_area");
+const foods = []// Empty array to store food objects
+const submit_Btn = document.getElementById("form_area")
+const calculate_highestBtn = document.getElementById("calculate_highest")
+const info_area = document.getElementById("info_area")
 
 submit_Btn.addEventListener("submit", function (event) {//add addEventListener for the submit button
     event.preventDefault();
 
-    const food_name = document.getElementById("food_name").value;//get the food value
-    const carbs = document.getElementById("carbs").value;//get the carb value
-    const fat = document.getElementById("fat").value;//get the fat value
-    const protein = document.getElementById("protein").value;//get the protein value
+    const food_name = document.getElementById("food_name").value//get the food value
+    const carbs = document.getElementById("carbs").value//get the carb value
+    const fat = document.getElementById("fat").value//get the fat value
+    const protein = document.getElementById("protein").value//get the protein value
 
-    
-    const food = new Food(food_name, carbs, fat, protein);// Create a Food object
-    Foods.push(food);// Push the object into the foods array
 
-    form_area.reset();// Reset form
+    const food = new Food(food_name, carbs, fat, protein)// Create a Food object
+    foods.push(food)// Push the object into the foods array
 
-    console.log(Foods)
-    console.log(typeof Foods[0])
-    console.log(Foods.length)
+    form_area.reset()// Reset form
+
+    info_area.innerHTML = ''
+    // info_area.reset()
+
+    const names = []
+
+    for (let i = 0; i < foods.length; i++){
+        // console.log(foods[i].name)
+        names.push(foods[i].name)
+        // console.log(names)
+        info_area.innerHTML = names.join(' ')
+    }
+
+    // console.log(foods)
+    // console.log(typeof foods[0])
+    // console.log(foods.length)
 });
 
 calculate_highestBtn.addEventListener("click", function (event) {//add addEventListener for the calculate button
-    event.preventDefault();
-    if(Foods.length > 0){//to check if the array is still empty
+    event.preventDefault()
+    const value = Food.gethi(foods)
 
-        let highest_Carb = Foods[0];//create varible to store the object highest_Carb
-        let highest_Fat = Foods[0];//create varible to store the object highest_Fat
-        let highest_Protein = Foods[0];//create varible to store the object highest_Protein
 
-        for (const food of Foods) {// Calculate the objects with highest values
-            if (food.carbs > highest_Carb.carbs) {
-                highest_Carb = food;
-            }
-            if (food.fat > highest_Fat.fat) {
-                highest_Fat = food;
-            }
-            if (food.protein > highest_Protein.protein) {
-                highest_Protein = food;
-            }
-        }
+    info_area.innerHTML = `Among (${foods.map(food => food.name).join(", ")}) <br>`
+        + `${value.highest_Carb.name} has the highest Carb <br>`
+        + `${value.highest_Fat.name} has the highest Fat <br>`
+        + `${value.highest_Protein.name} has the highest Protein <br>`//List the food with highest values
 
-        info_area.innerHTML = `Among (${Foods.map(food => food.name).join(", ")}) <br>`
-                            + `${highest_Carb.name} has the highest Carb <br>`
-                            + `${highest_Fat.name} has the highest Fat <br>`
-                            + `${highest_Protein.name} has the highest Protein <br>`;//List the food with highest values
-    }else{
-        window.alert("No food item stored yet.")
-    }
-});
+})
